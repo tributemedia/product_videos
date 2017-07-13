0bb3bb516e2401f09e30fc9736b6ab44650c42a1
Tribute Media <servers@tributemedia.com>
2017-07-13 15:36:34 -0600

    Add Module files

---
+++ js/prod-vid.js
@@ -0,0 +1,46 @@
+;(function($){
+  
+  function closeLightbox() {
+    $('#video-frame iframe').attr('src', '');
+    $('#product-video-lightbox').addClass('hide');
+    $('#pvl-overlay').addClass('hide');
+  }
+  
+$(document).ready(function() {
+  
+  // Accordian stuffs
+  
+  $('.video-group').addClass('closed');
+  $('.view-product-videos .view-content .video-category:first .video-group').removeClass('closed').addClass('open');
+  $('.video-group.closed').toggle();
+  
+  $('.video-category h3').click(function() {
+    $('.video-group').addClass('closed');
+    $('.video-group.open').animate({
+      height: "toggle"
+    }, 500);
+    $('.video-group.open').removeClass('open');
+    $(this).siblings('.video-group').removeClass('closed').addClass('open');
+    $(this).siblings('.video-group').animate({
+      height: "toggle"
+    }, 500);
+  });
+  
+  // Lightbox stuffs
+  
+  $('.video-lightbox').click(function() {
+    var videoSrc = $(this).attr('vid-src');
+    videoSrc = videoSrc.replace('watch?v=', 'embed/') + '?rel=0';
+    $('#video-frame iframe').attr('src', videoSrc);
+    $('#pvl-overlay').removeClass('hide');
+    $('#product-video-lightbox').removeClass('hide');
+  });
+  $('#close-box').click(function() {
+    closeLightbox();
+  });
+  $('#pvl-overlay').click(function() {
+    closeLightbox();
+  });
+});
+
+})(jQuery);
